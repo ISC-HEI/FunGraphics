7345f3f5abfe3d8332885b1cdad87b9110a474dc
Marc Pignat <marc@pignat.org>
2022-11-23 09:17:12 +0100

    add StringFunctions

---
+++ src/main/scala/hevs/utils/StringFunctions.scala
@@ -0,0 +1,24 @@
+package hevs.utils
+
+object StringFunctions {
+  /**
+   * Get the number of characters in a String
+   *
+   * @param s the string
+   * @return The length (number of characters) of the String s
+   */
+  def stringLength(s: String): Int = s.length
+
+  /**
+   * Get one character from a String
+   *
+   * Example :
+   * stringCharAt("hello", 0) returns 'h'
+   * stringCharAt("hello", 1) returns 'e'
+   *
+   * @param s the String
+   * @param pos the position of the character
+   * @return The char at position pos in s
+   */
+  def stringCharAt(s: String, pos: Int): Char = s.charAt(pos)
+}
